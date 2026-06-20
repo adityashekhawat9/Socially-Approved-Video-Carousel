885214a515552731a43fb442b851f145d5046bf5
Aditya Shekhawat <adityasinghshekhawat25@gmail.com>
2026-06-20 11:10:14 +0530

    Allow Vercel frontend CORS origin

--- server/index.js
+++ server/index.js
@@ -13,9 +13,24 @@ const shareRoutes = require('./routes/shares');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const allowedOrigins = [
+  'http://localhost:3000',
+  'https://socially-approved-video-carousel-blush.vercel.app',
+  process.env.CLIENT_URL,
+].filter(Boolean);
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin(origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    }
+
+    return callback(new Error(`CORS blocked for origin: ${origin}`));
+  },
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -52,4 +67,3 @@ connectDB().then(() => {
   console.error('Failed to connect to database. Server not started.');
   process.exit(1);
 });
-
